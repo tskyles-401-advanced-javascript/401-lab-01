bb44ac7b5b329be5c7ee2863a26dffd540093799
travis skyles <travis.skyles@gmail.com>
2020-01-06 23:08:28 -0800

    ran jsdoc

--- index.js
+++ index.js
@@ -3,3 +3,15 @@
 const validator = require('./lib/validator.js');
 
 validator.isValid();
+
+const susan = {
+  id: '123-45-6789',
+  name: 'Susan McDeveloperson',
+  age: 37,
+  children: []
+};
+const fred = {
+  id: 38,
+  name: 'Freddy McCoder',
+  children: []
+};

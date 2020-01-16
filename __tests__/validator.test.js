740a9a8a7623e6bcb80b5d1152940dae6756d64f
travis skyles <travis.skyles@gmail.com>
2020-01-15 20:32:24 -0800

    tests passing

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -3,14 +3,25 @@
 const validator = require('../lib/validator.js');
 const faker = require('faker');
 
+let str = 'yes';
+let num = 1;
+let arr = ['a'];
+let obj = { x: 'y' };
+let func = () => {};
+let bool = false;
+
+const schema = {
+  fields: {
+    id: { type: 'string', required: true },
+    name: { type: 'string', required: true },
+    age: { type: 'number', required: true },
+    children: { type: 'array', valueType: 'string' },
+  },
+};
+
 describe('validator module performs basic validation of', () => {
   // TODO: Make this series of tests less repetitive ... DRY it out
-  let str = 'yes';
-  let num = 1;
-  let arr = ['a'];
-  let obj = { x: 'y' };
-  let func = () => {};
-  let bool = false;
+
   it('strings', () => {
     expect(validator.isString(str)).toBeTruthy();
     expect(validator.isString(num)).toBeFalsy();
@@ -76,49 +87,50 @@ describe('validator module performs complex validations', () => {
     //go through the schema and fill in perfect values for every field
     let testRecord = {};
 
-    for (let field in rules.fields) {
-      switch (rules.fields[field].type) {
-        case 'boolean':
-          testRecord[field] = faker.random.boolean();
-          break;
-        case 'number':
-          testRecord[field] = faker.random.number();
-          break;
-        case 'string':
-          testRecord[field] = faker.random.word();
-          break;
-        case 'array':
-          testRecord[field] = [];
-          testRecord[field].push(faker.random.arrayElement());
-          testRecord[field].push(faker.random.arrayElement());
-          break;
-        default:
-          null;
+    for (let field in schema.fields) {
+      switch (schema.fields[field].type) {
+      case 'boolean':
+        testRecord[field] = faker.random.boolean();
+        break;
+      case 'number':
+        testRecord[field] = faker.random.number();
+        break;
+      case 'string':
+        testRecord[field] = faker.random.word();
+        break;
+      case 'array':
+        testRecord[field] = [];
+        testRecord[field].push(faker.random.arrayElement());
+        testRecord[field].push(faker.random.arrayElement());
+        break;
+      default:
+        null;
       }
     }
-    expect(validator.isValid(testRecord, rules)).toBeTruthy();
-  });
-
-  it('validates the presence of required object properties at any level', () => {
-    // i.e. does person.hair.color exist and have a good value, not just person.hair
-
-    expect(true).toBeFalsy();
+    expect(validator.isValid(testRecord, schema)).toBeTruthy();
   });
 
   it('validates the proper types of object properties', () => {
     // i.e. person.name must be a string, etc.
-    expect(true).toBeFalsy();
+    const test = {
+      id: '123-45-6789',
+      name: 'Susan McDeveloperson',
+      age: 37,
+      children: [],
+    };
+    expect(validator.isString(test.name)).toBeTruthy();
+    expect(validator.isArray(test.children)).toBeTruthy();
+    expect(validator.isNumber(test.age)).toBeTruthy();
+    expect(validator.isString(test.id)).toBeTruthy();
   });
 
-  it('validates the types of values contained in an array', () => {
-    // i.e. an array of all strings or numbers
-    expect(true).toBeFalsy();
+  it('returns false if object is not valid', () => {
+    const testBad = {
+      id: 5,
+      name: ['Susan McDeveloperson'],
+      age: '37',
+      children: 'no',
+    };
+    expect(validator.isValid(testBad, schema)).toBeFalsy();
   });
-
-  it('validates a value array against an approved list', () => {
-    // i.e. a string might only be allowed to be "yes" or "no"
-    expect(true).toBeFalsy();
-  });
-
-  // TODO: Cover so, so many more cases
 });
